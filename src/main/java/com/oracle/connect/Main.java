e48cf847f236a36d061fd571f42a92a2c1cd0a2c
LLEFEVRE <loic.lefevre@gmail.com>
2024-04-04 09:33:51 +0200

    first commit

--- src/main/java/com/oracle/connect/Main.java
+++ src/main/java/com/oracle/connect/Main.java
@@ -1,7 +1,34 @@
 package com.oracle.connect;
 
-public class Main {
-	public static void main(String[] args) {
+import com.oracle.connect.gui.Controller;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.stage.Stage;
 
+import java.util.Locale;
+
+public class Main extends javafx.application.Application {
+	static Controller guiController;
+
+	public static void main(String[] args) {
+		System.setProperty("prism.lcdtext", "false");
+		Locale.setDefault(Locale.US);
+
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		final FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+		Parent parent = fxmlLoader.load();
+		final Scene scene = new Scene(parent, -1f, -1f, false, SceneAntialiasing.BALANCED);
+		stage.setResizable(false);
+		stage.setScene(scene);
+
+		guiController = fxmlLoader.getController();
+		guiController.initialize();
+		stage.show();
 	}
 }
---
+++ src/main/java/com/oracle/connect/gui/Controller.java
@@ -0,0 +1,11 @@
+package com.oracle.connect.gui;
+
+public class Controller {
+	private boolean needsInitialization = true;
+
+	public void initialize() {
+		if (needsInitialization) {
+
+		}
+	}
+}
